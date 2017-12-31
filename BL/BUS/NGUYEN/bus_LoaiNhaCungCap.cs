4aeac685a37f8344306e43fc206f6c09ad9f073f
phamhoainguyen <phamhoainguyen171096@gmail.com>
2017-12-31 10:06:01 +0700

    commit - bus_LoaiNhaCungCap bus_NhaCungCap

--- BL/BUS/NGUYEN/bus_LoaiNhaCungCap.cs
+++ BL/BUS/NGUYEN/bus_LoaiNhaCungCap.cs
@@ -2,6 +2,7 @@
 using BL.VO.NGUYEN;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Linq;
 using System.Text;
@@ -16,8 +17,8 @@ namespace BL.BUS.NGUYEN
             try
             {
 
-                dao_LoaiNhaCungCap dal = new dao_LoaiNhaCungCap();
-                int value = dal.insertLoaiNCC(vo_loaiNCC);
+                dao_LoaiNhaCungCap dao = new dao_LoaiNhaCungCap();
+                int value = dao.insertLoaiNCC(vo_loaiNCC);
                 return value;
             }
             catch (Exception ex)
@@ -30,14 +31,27 @@ namespace BL.BUS.NGUYEN
         {
             try
             {
-                dao_LoaiNhaCungCap dal = new dao_LoaiNhaCungCap();
-                int value = dal.GetLastestIDLoaiNCC();
+                dao_LoaiNhaCungCap dao = new dao_LoaiNhaCungCap();
+                int value = dao.GetLastestIDLoaiNCC();
                 return value;
             }
             catch (Exception ex)
             {
                 throw ex;
             }
+        }
+
+        public ObservableCollection<vo_LoaiNhaCungCap> getListLoaiNhaCungCap()
+        {
+            dao_LoaiNhaCungCap dao = new dao_LoaiNhaCungCap();
+            DataTable dt = dao.getAllLoaiNhaCungCap();
+            ObservableCollection<vo_LoaiNhaCungCap> dsLoaiNCC = new ObservableCollection<vo_LoaiNhaCungCap>();
+            foreach (DataRow row in dt.Rows)
+            {
+                vo_LoaiNhaCungCap vo = new vo_LoaiNhaCungCap(int.Parse(row["ID"].ToString()), row["TENLOAINHACUNGCAP"].ToString());
+                dsLoaiNCC.Add(vo);
+            }
+            return dsLoaiNCC;
         }
     }
 }
---
+++ BL/BUS/NGUYEN/bus_NhaCungCap.cs
@@ -0,0 +1,36 @@
+﻿using BL.DAO.NGUYEN;
+using BL.VO.NGUYEN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.BUS.NGUYEN
+{
+    public class bus_NhaCungCap
+    {
+        public int addNhaCungCap(vo_NhaCungCap vo_nhaCC)
+        {
+            dao_NhaCungCap dao_nhaCungCap = new dao_NhaCungCap();
+            int id = dao_nhaCungCap.insertNhaCungCap(vo_nhaCC);
+            return id;
+        }
+
+        public string generateMaNhaCungCap()
+        {
+            try
+            {
+                dao_NhaCungCap dao_nhaCC = new dao_NhaCungCap();
+                int id = dao_nhaCC.getLastestNhaCungCapId() + 1;
+                string code = "NCC" + String.Format("{0:00000}", id);
+                return code;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
