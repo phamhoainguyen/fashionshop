e550b8c1174844ab8649ba4751240767638c9fd2
hoanghuy <14520362@gm.uit.edu.vn>
2018-01-06 09:05:58 +0700

    page Danh Sach Khach Hang

--- PL/HUY/pl_pageDanhSachKhachHang.xaml.cs
+++ PL/HUY/pl_pageDanhSachKhachHang.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BL.BUS.HUY;
+using BL.VO.HUY;
+using System.Collections.ObjectModel;
 
 namespace PL.HUY
 {
@@ -20,9 +23,33 @@ namespace PL.HUY
     /// </summary>
     public partial class pl_pageDanhSachKhachHang : Page
     {
+        private bus_KhachHang bus_KH;
+        private ObservableCollection<vo_KhachHang> dsKhachHang;
         public pl_pageDanhSachKhachHang()
         {
-            InitializeComponent();
+            try
+            {
+                this.bus_KH = new bus_KhachHang();
+                InitializeComponent();
+                this.dsKhachHang = bus_KH.getAllKhachHang();
+                this.DataContext = this;
+                this.lvKhachHang.ItemsSource = this.dsKhachHang;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Loi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
+        private void lvKhachHang_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Double click", "Loi!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void TableView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Double click", "Loi!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+            
+        
     }
 }
