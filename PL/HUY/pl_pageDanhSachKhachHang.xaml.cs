6d93c0ecab0b42e5b7297edbf0e236f2a3f94830
hoanghuy <14520362@gm.uit.edu.vn>
2018-01-07 12:36:06 +0700

    danh sach khach hang

--- PL/HUY/pl_pageDanhSachKhachHang.xaml.cs
+++ PL/HUY/pl_pageDanhSachKhachHang.xaml.cs
@@ -49,7 +49,52 @@ namespace PL.HUY
         {
             MessageBox.Show("Double click", "Loi!", MessageBoxButton.OK, MessageBoxImage.Error);
         }
-            
-        
+
+        private void btnThemKhachHang_Click(object sender, RoutedEventArgs e)
+        {
+            try
+
+            {
+                pl_windowThemKhachHang pl_ThemKH = new pl_windowThemKhachHang();
+                pl_ThemKH.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Loi!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        private void maKhangHang_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                ObservableCollection<vo_KhachHang> dsKHCopy = this.bus_KH.getAllKhachHang();
+                ObservableCollection<vo_KhachHang> temp = new ObservableCollection<vo_KhachHang>();
+                if (string.IsNullOrEmpty(this.maKhangHang.Text))
+                {
+                    this.lvKhachHang.ItemsSource = dsKHCopy;
+                }
+                else
+                {
+                    string _text = this.maKhangHang.Text;
+                    foreach (vo_KhachHang _vo in dsKHCopy)
+                    {
+                        if  (_vo.MaKhachHang.ToLower().Contains(_text.ToLower()) || _vo.TenKhachHang.ToLower().Contains(_text.ToLower()))
+                        {
+                            temp.Add(_vo);
+                        }
+                    }
+                    this.lvKhachHang.ItemsSource = temp;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Loi!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void lvKhachHang_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
