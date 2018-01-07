f7a97724bf8e14e229adc70b0a5bd6be0c799d5e
hoanghuy <14520362@gm.uit.edu.vn>
2018-01-07 16:43:09 +0700

    dang nhap

---
+++ PL/HUY/pl_windowDangNhap.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.HUY
+{
+    /// <summary>
+    /// Interaction logic for pl_windowDangNhap.xaml
+    /// </summary>
+    public partial class pl_windowDangNhap : Window
+    {
+        public pl_windowDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangNhap_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
