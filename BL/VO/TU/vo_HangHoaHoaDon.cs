5647cd22f0b1aec18fdd4fd0e8e2494b36159c82
Nuemotion12 <nuemotiononetwo@gmail.com>
2018-01-09 20:31:22 +0700

    Vo- HangHoaHoaDon Vo-HoaDon

---
+++ BL/VO/TU/vo_HangHoaHoaDon.cs
@@ -0,0 +1,219 @@
+﻿using BL.bl_Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.VO.TU
+{
+    public class vo_HangHoaHoaDon : PropertyChangedBase
+    {
+        public vo_HangHoaHoaDon(string _maHangHoa, string _tenHangHoa, int _soLuong, int _donGia, int _giaGiam,int _tonKhoCu)
+        {
+            this.maHangHoa = _maHangHoa;
+            this.soLuong = _soLuong;
+            this.tenHangHoa = _tenHangHoa;
+            this.donGia = _donGia;
+            this.thanhTien = this.donGia;
+            this.giaGiam = _giaGiam;
+            this.tongGiamChiTiet = this.giaGiam;
+            this.tonKhoCu = _tonKhoCu;
+            this.tonKho = tonKhoCu - 1;
+        }
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+        private string maHangHoa;
+        public string MaHangHoa
+        {
+            get { return maHangHoa; }
+            set
+            {
+                if (maHangHoa != value)
+                {
+                    maHangHoa = value;
+                    OnPropertyChanged("MaHangHoa");
+                }
+            }
+        }
+        private string tenHangHoa;
+        public string TenHangHoa
+        {
+            get { return tenHangHoa; }
+            set
+            {
+                if (tenHangHoa != value)
+                {
+                    tenHangHoa = value;
+                    OnPropertyChanged("TenHangHoa");
+                }
+            }
+        }
+        private int donGia;
+        public int DonGia
+        {
+            get { return donGia; }
+            set
+            {
+                if (donGia != value)
+                {
+                    donGia = value;
+
+                    OnPropertyChanged("DonGia");
+                }
+            }
+        }
+        private int giaVon;
+        public int GiaVon
+        {
+            get { return giaVon; }
+            set
+            {
+                if (giaVon != value)
+                {
+                    giaVon = value;
+                    OnPropertyChanged("GiaVon");
+                }
+            }
+        }
+        private int soLuong;
+        public int SoLuong
+        {
+            get { return soLuong; }
+            set
+            {
+                if (soLuong != value)
+                {
+                    soLuong = value;
+                    this.thanhTien = this.donGia * value;
+                    this.TongGiamChiTiet = this.giaGiam * value;
+                    this.TonKho = this.tonKhoCu - value;
+                    OnPropertyChanged("SoLuong");
+                }
+            }
+        }
+
+        private string urlImage;
+        public string UrlImage
+        {
+            get { return urlImage; }
+            set
+            {
+                if (urlImage != value)
+                {
+                    urlImage = value;
+                    OnPropertyChanged("UrlImage");
+                }
+            }
+        }
+        private string ghiChu;
+        public string GhiChu
+        {
+            get { return ghiChu; }
+            set
+            {
+                if (ghiChu != value)
+                {
+                    ghiChu = value;
+                    OnPropertyChanged("GhiChu");
+                }
+            }
+        }
+
+        private int idLoaiHangHoa;
+        public int IdLoaiHangHoa
+        {
+            get { return idLoaiHangHoa; }
+            set
+            {
+                if (idLoaiHangHoa != value)
+                {
+                    idLoaiHangHoa = value;
+                    OnPropertyChanged("IdLoaiHangHoa");
+                }
+            }
+        }
+
+        private string loaiHangHoa;
+        public string LoaiHangHoa
+        {
+            get { return loaiHangHoa; }
+            set
+            {
+                if (loaiHangHoa != value)
+                {
+                    loaiHangHoa = value;
+                    OnPropertyChanged("LoaiHangHoa");
+                }
+            }
+        }
+
+        private int giaGiam;
+        public int GiaGiam
+        {
+            get { return giaGiam; }
+            set
+            {
+                if (giaGiam != value)
+                {
+                    giaGiam = value;
+                    OnPropertyChanged("GiaGiam");
+                }
+            }
+        }
+        public int thanhTien;
+        public int ThanhTien
+        {
+            get { return thanhTien; }
+            set
+            {
+                if (thanhTien != value)
+                {
+                    thanhTien = value;
+                    OnPropertyChanged("ThanhTien");
+                }
+            }
+        }
+        public int tongGiamChiTiet;
+        public int TongGiamChiTiet
+        {
+            get { return tongGiamChiTiet; }
+            set
+            {
+                if (tongGiamChiTiet != value)
+                {
+                    tongGiamChiTiet = value;
+                    OnPropertyChanged("TongGiamChiTiet");
+                }
+            }
+        }
+
+        public int tonKho;
+        public int TonKho
+        {
+            get { return tonKho; }
+            set
+            {
+                if (tonKho != value)
+                {
+                    tonKho = value;
+                    OnPropertyChanged("TonKho ");
+                }
+            }
+        }
+        public int tonKhoCu;
+
+    }
+}
+
---
+++ BL/VO/TU/vo_HoaDon.cs
@@ -0,0 +1,256 @@
+﻿using BL.bl_Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.VO.TU
+{
+    public class vo_HoaDon : PropertyChangedBase
+    {
+        private String maHoaDon;
+        private String maKhachHang;
+        private String tenKhachHang;
+        private String thoiGian;
+        private int canThanhToan;
+        private int tongGiam;
+        private int datra;
+        //số tiền cần trả sau khi giảm giá
+        private int tongTienHang;
+        //tiền thừa hoặc tiền nợ
+        private int conNo;
+        private String maNhanVien;
+        private String tenNhanVien;
+        private String ghiChu ;
+
+        private ObservableCollection<vo_HangHoaHoaDon> dsHangHoaHoaDon;
+
+
+
+        public vo_HoaDon()
+        {
+            GhiChu = "";
+        }
+
+        public vo_HoaDon(String _maHoaDon, String _maKhachHang, String _tenKhachHang, String _thoiGian, int _canThanhToan, int _tongGiam, int _daTra)
+        {
+            this.maHoaDon = _maHoaDon;
+            this.maKhachHang = _maKhachHang;
+            this.tenKhachHang = _tenKhachHang;
+            this.thoiGian = _thoiGian;
+            this.canThanhToan = _canThanhToan;
+            this.tongGiam = _tongGiam;
+            this.datra = _daTra;
+
+        }
+
+        public String MaHoaDon
+        {
+            get { return maHoaDon; }
+            set
+            {
+                if (value != maHoaDon)
+                {
+                    maHoaDon = value;
+                    OnPropertyChanged("MaHoaDon");
+                }
+            }
+
+
+        }
+
+        public string ThoiGian
+        {
+            get { return thoiGian; }
+            set
+            {
+                if (value != thoiGian)
+                {
+                    thoiGian = value;
+                    OnPropertyChanged("MaHoaDon");
+                }
+            }
+
+
+        }
+
+        public string TenKhachHang
+        {
+            get { return tenKhachHang; }
+            set
+            {
+                if (value != tenKhachHang)
+                {
+                    tenKhachHang = value;
+                    OnPropertyChanged("TenKhachHang");
+                }
+            }
+
+
+        }
+
+        public string MaKhachHang
+        {
+            get { return maKhachHang; }
+            set
+            {
+                if (value != maKhachHang)
+                {
+                    maKhachHang = value;
+                    OnPropertyChanged("MaKhachHang");
+                }
+            }
+
+
+        }
+
+        public string TenNhanVien
+        {
+            get { return tenNhanVien; }
+            set
+            {
+                if (value != tenNhanVien)
+                {
+                    tenNhanVien = value;
+                    OnPropertyChanged("TenNhanVien");
+                }
+            }
+
+
+        }
+
+        public string MaNhanVien
+        {
+            get { return maNhanVien; }
+            set
+            {
+                if (value != maNhanVien)
+                {
+                    maNhanVien = value;
+                    OnPropertyChanged("MaNhanVien");
+                }
+            }
+
+
+        }
+
+        public int CanThanhToan
+        {
+            get { return canThanhToan; }
+            set
+            {
+                if (value != canThanhToan)
+                {
+                    this.canThanhToan = value;
+
+                    OnPropertyChanged("CanThanhToan");
+                }
+            }
+
+
+        }
+
+        public int TongGiam
+        {
+            get { return tongGiam; }
+            set
+            {
+                if (value != tongGiam)
+                {
+                    this.tongGiam = value;
+                    CanThanhToan = tongTienHang - tongGiam;
+                    OnPropertyChanged("TongGiam");
+                }
+            }
+
+
+        }
+
+        public int DaTra
+        {
+            get { return datra; }
+            set
+            {
+                if (value != datra)
+                {
+                    datra = value;
+                    ConNo = this.datra - this.canThanhToan;
+                    OnPropertyChanged("TenKhacHang");
+                }
+            }
+
+
+        }
+
+        public int TongTienHang
+        {
+            get { return tongTienHang; }
+            set
+            {
+                if (value != tongTienHang)
+                {
+                    tongTienHang = value;
+                    CanThanhToan = tongTienHang - tongGiam;
+                    OnPropertyChanged("TongTienHang");
+                }
+            }
+
+
+        }
+
+        public int ConNo
+        {
+            get { return conNo; }
+            set
+            {
+                if (value != conNo)
+                {
+                    conNo = value;
+                    OnPropertyChanged("ConNo");
+                }
+            }
+
+
+        }
+
+
+        public string GhiChu
+        {
+            get { return ghiChu; }
+            set
+            {
+                if (value != ghiChu)
+                {
+                    ghiChu = value;
+                    OnPropertyChanged("GhiChu");
+                }
+            }
+
+
+        }
+
+        public ObservableCollection<vo_HangHoaHoaDon> DsHangHoaHoaDon
+        {
+            get { return dsHangHoaHoaDon; }
+            set
+            {
+                if (dsHangHoaHoaDon != value)
+                {
+                    dsHangHoaHoaDon = value;
+                    OnPropertyChanged("DsHangHoaHoaDon");
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
