09f0fe630b4f47ddff794f5297131ee042c26eb7
phamhoainguyen <phamhoainguyen171096@gmail.com>
2018-01-11 14:08:31 +0700

    Main

--- PL/Main.xaml.cs
+++ PL/Main.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PL.HUY;
+using PL.NGUYEN;
+using PL.TU;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,11 +25,120 @@ namespace PL
         public Main()
         {
             InitializeComponent();
+            this.contentArea.Content = new pl_pageThemHoaDon();
         }
 
         private void navBarGroup1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void navBarItemNhapHang_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pl_pagePhieuNhap pagePhieuNhap = new pl_pagePhieuNhap();
+                
+                contentArea.Content = pagePhieuNhap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Loi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void navBarItemDanhMuc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pl_pageHangHoa pageHangHoa = new pl_pageHangHoa();
+                contentArea.Content = pageHangHoa;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Loi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void navBarItemThemMoi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void navBarItemThietLapGia_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void navBarItemDSNhanVien_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pl_pageNhanVien pageNV = new pl_pageNhanVien();
+                contentArea.Content = pageNV;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void navBarItemLuong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void navBarItemNCC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pl_pageNhaCungCap pageNCC = new pl_pageNhaCungCap();
+                contentArea.Content = pageNCC;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+
+        private void navBarItemKhachHang_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pl_pageDanhSachKhachHang pageKH = new pl_pageDanhSachKhachHang();
+                contentArea.Content = pageKH;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void navBarItemBanHang_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pl_pageThemHoaDon pageHD = new pl_pageThemHoaDon();
+                contentArea.Content = pageHD;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void navBarItemCuaHang_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pl_pageGioiThieu pageGT = new pl_pageGioiThieu();
+                contentArea.Content = pageGT;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
